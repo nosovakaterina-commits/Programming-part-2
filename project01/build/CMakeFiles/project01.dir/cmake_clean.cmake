613fdae09a767c615937dec4b5423a73c9df86cb
Катерина <nosovakaterina>
2026-03-20 13:50:54 +0000

    Add branch lab01

---
+++ project01/build/CMakeFiles/project01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project01.dir/main.cpp.o"
+  "CMakeFiles/project01.dir/main.cpp.o.d"
+  "project01"
+  "project01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
