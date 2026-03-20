541ef8bdff90912265b1dfe1203729b7b0f7b53c
Катерина <nosovakaterina>
2026-03-20 22:22:27 +0000

    Add project03 and project04

---
+++ project04/app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "math_utils.hpp"
+
+int main() {
+    std::cout << "2 + 3 = " << add(2, 3) <<std::endl;
+    std::cout << "7 - 4 = " << sub(7, 4) <<std::endl;
+    return 0;
+}
