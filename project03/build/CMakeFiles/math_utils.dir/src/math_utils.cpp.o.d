ba83b53b17bdb9d63cf072527e6c801046715190
Катерина <nosovakaterina>
2026-03-20 21:26:26 +0000

    Remove build folder and add gitignore

---
+++ project03/build/CMakeFiles/math_utils.dir/src/math_utils.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/math_utils.dir/src/math_utils.cpp.o: \
+ /home/cer_nosova_kate/programming-part-2/project03/src/math_utils.cpp \
+ /usr/include/stdc-predef.h \
+ /home/cer_nosova_kate/programming-part-2/project03/include/math_utils.hpp
---
+++ project03/build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cer_nosova_kate/programming-part-2/project03/build/_deps/googletest-src
+# Build directory: /home/cer_nosova_kate/programming-part-2/project03/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
