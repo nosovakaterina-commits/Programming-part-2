fd284363b2783033b59857b4b7bbcce298a93362
Катерина <nosovakaterina>
2026-06-06 08:51:38 +0000

    Lab 11 corrected version

--- lab11/include/grade_threshold.h
+++ lab11/include/grade_threshold.h
@@ -2,14 +2,16 @@
 
 // Базовий шаблон для цілих оцінок (int)
 template <typename T>
-struct GradeThreshold {
+struct GradeThreshold
+{
     static constexpr T pass() { return static_cast<T>(60); }
     static constexpr T excellent() { return static_cast<T>(90); }
 };
 
 // Повна спеціалізація шаблону для типу double
 template <>
-struct GradeThreshold<double> {
+struct GradeThreshold<double>
+{
     static constexpr double pass() { return 60.0; }
     static constexpr double excellent() { return 90.0; }
 };
--- lab11/include/named_record.h
+++ lab11/include/named_record.h
@@ -2,7 +2,8 @@
 
 // Шаблон класу з двома незалежними параметрами типу
 template <typename NameType, typename ValueType>
-class NamedRecord {
+class NamedRecord
+{
 private:
     NameType name_;
     ValueType value_;
@@ -11,12 +12,12 @@ public:
     NamedRecord() : name_(), value_() {}
 
     // Конструктор з параметрами
-    NamedRecord(const NameType& name, const ValueType& value) 
+    NamedRecord(const NameType &name, const ValueType &value)
         : name_(name), value_(value) {}
 
-        NameType getName() const { return name_; }
+    NameType getName() const { return name_; }
     ValueType getValue() const { return value_; }
 
-    void setName(const NameType& name) { name_ = name; }
-    void setValue(const ValueType& value) { value_ = value; }
+    void setName(const NameType &name) { name_ = name; }
+    void setValue(const ValueType &value) { value_ = value; }
 };
